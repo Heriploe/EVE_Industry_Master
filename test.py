b3c0fcbef47e7df56f71a81b9cd5d26faab81768
Heriploe <zhouyuyang@gmail.com>
2026-03-02 03:23:12 -0800

    233

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from Utilities.industry_cost import get_activity_cost
+from Utilities.industry_cost import get_activity_cost, get_base_cost
 
 
 if __name__ == "__main__":
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     runs = 1
     activity = "manufacturing"
     cost = get_activity_cost(blueprint_id=blueprint_id, runs=runs, activity=activity)
-    print(f"blueprint_id={blueprint_id}, activity={activity}, runs={runs}, cost={cost}")
+    base_cost = get_base_cost(blueprint_id=blueprint_id, activity=activity)
+    print(f"blueprint_id={blueprint_id}, activity={activity}, runs={runs}, cost={cost},base_cost={base_cost}")
